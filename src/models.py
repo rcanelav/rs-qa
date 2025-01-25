9d2771989ba423314fed036e2423a4b5c9f09f57
Ramón Canela Villamizar <rcanelav@gmail.com>
2025-01-25 21:28:51 +0100

    UPDATE: questionanswer model docstring

--- src/models.py
+++ src/models.py
@@ -2,4 +2,9 @@ from pydantic import BaseModel
 
 
 class QuestionAnswerRequest(BaseModel):
+    """Checks request payload for later processing
+
+    Args:
+        BaseModel (BaseModel): Pydantic's BaseModel class.
+    """
     question: str
